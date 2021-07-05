768ffdaa6433a939a5b0dacf4c4e4c46c970e675
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-05 22:26:39 +0200

    Add routes

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,4 @@
-# from exception.exceptions import InvalidAuthException
+from exception.exceptions import InvalidAuthException
 import time
 from os import environ
 from datetime import datetime
@@ -9,17 +9,17 @@ def check(obj: dict) -> bool:
     return not obj or any(value is None or ((type(value) is list or (type(value) is str and key != 'description')) and len(value) == 0) or ((type(value) is int or type(value) is float) and value < 1) for key, value in obj.items())
 
 
-# def auth(token: str):
-#     try:
-#         payload = jwt.decode(token, environ.get('JWT_SECRET'), environ.get('JWT_ALGORITHM'))
-#     except Exception:
-#         raise InvalidAuthException('Forbidden, invalid authentication.')
+def auth(token: str):
+    try:
+        payload = jwt.decode(token, environ.get('JWT_SECRET'), environ.get('JWT_ALGORITHM'))
+    except Exception:
+        raise InvalidAuthException('Forbidden, invalid authentication.')
 
-#     exp = payload['exp']
-#     if datetime.now() > datetime.fromtimestamp(exp / 1000.0):
-#         raise InvalidAuthException('Forbidden, authorization token has expired.')
+    exp = payload['exp']
+    if datetime.now() > datetime.fromtimestamp(exp / 1000.0):
+        raise InvalidAuthException('Forbidden, authorization token has expired.')
 
-#     return payload
+    return payload
 
 
 # def generate_token(user: dict):
---
+++ controller/campaign_controller.py
@@ -0,0 +1,59 @@
+from exception.exceptions import InvalidAuthException, InvalidDataException, NotFoundException
+from flask_restful import Resource, reqparse
+from flask import request
+from common.utils import auth
+# from service import campaign_service
+
+campaign_parser = reqparse.RequestParser()
+campaign_parser.add_argument('Authorization', type=str, location='headers', required=True)
+campaign_parser.add_argument('description', type=str, help='Description for campaign')
+campaign_parser.add_argument('image_url', type=str, help='Image url for campaign')
+campaign_parser.add_argument('interests', type=str, help='Interests for campaign')
+campaign_parser.add_argument('age_min', type=int, help='Min. age for campaign')
+campaign_parser.add_argument('age_max', type=int, help='Max. age for campaign')
+campaign_parser.add_argument('regions', type=str, help='Regions for campaign')
+campaign_parser.add_argument('sex', type=str, help='Sex for age')
+
+ 
+class CampaignResource(Resource):
+    def __init__(self):
+        # To be implemented.
+        pass
+ 
+    def get(self, campaign_id):        
+        try:
+            # campaign_service.get(campaign_id), 200
+            pass
+        except NotFoundException as e:
+            return str(e), 404
+
+class PostCampaignResource(Resource):
+    def __init__(self):
+        # To be implemented.
+        pass
+ 
+    def post(self):
+        args = campaign_parser.parse_args()
+        token = args['Authorization'].split(' ')[1]
+        del args['Authorization']
+
+        try:
+            user = auth(token)
+
+            # return campaign_service.create(args, user), 200
+        except InvalidAuthException as e:
+            return str(e), 401
+        except InvalidDataException as e:
+            return str(e), 400
+ 
+class UserResource(Resource):
+    def __init__(self):
+        # To be implemented.
+        pass
+ 
+    def get(self, user_id):
+        page = int(request.args.get('page')) if request.args.get('page') else 1
+        per_page = int(request.args.get('per_page')) if request.args.get('per_page') else 10
+
+        # return campaign_service.get_with_user(user_id, page, per_page), 200
+ 
